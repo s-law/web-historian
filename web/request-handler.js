d8722fe6045ea6cc61e3e2c0a7491934aa21c0af
s-law <slaw@nuclearanvil.com>
2015-12-16 17:34:13 -0800

    Wrote out a skeletal request handler. Mostly uses helper functions; will probably need further refactoring.

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,40 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers');
+var url = require('url');
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+exports.handleRequest = function(req, res) {
+  var headers = httpHelpers.headers;
+
+  // .writeHead() writes to the request line and headers of the response,
+  // which includes the status and all headers.
+  var parsed = url.parse(req.url, true);
+  var route = parsed.pathname;
+
+  if (req.method === 'OPTIONS') {
+    httpHelpers.sendResponse(res, null);
+  } else if (req.method === 'GET') {
+    httpHelpers.serveAssets(res, req.url);
+  } else if (req.method === 'POST') {
+    httpHelpers.collectData(req)
+  //   var dataHolder = '';
+  //   req.on('data', function(data) {
+  //     dataHolder += data;
+  //   });
+
+  //   req.on('end', function() {
+  //     dataStore.results.push(JSON.parse(dataHolder));
+
+  //     // NOTE: writes are being APPENDED. each write is
+  //     // the previously stringified message obj
+  //     fs.appendFile(logPath, '\n' + dataHolder, function (err) {});
+
+  //     response.writeHead(201, headers);
+  //     response.end();
+  //   });
+  } else {
+    httpHelpers.sendResponse(res, null, 404);
+  }
+  // res.end(archive.paths.list);
 };
