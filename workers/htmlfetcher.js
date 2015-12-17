582cd63627b5d3cd44f50da4070be7879ce98a2d
s-law <slaw@nuclearanvil.com>
2015-12-16 20:28:08 -0800

    Added test urls.

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,5 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var archiveHelpers = require('../helpers/archive-helpers');
+
+archiveHelpers.downloadUrls(['www.google.com', 'www.nytimes.com']);
