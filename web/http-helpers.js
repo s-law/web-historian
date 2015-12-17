eaae64c31dc7200786ed09e88de28cf49cb2ba44
s-law <slaw@nuclearanvil.com>
2015-12-17 12:32:08 -0800

    Wrote out data processing helper function. Completed data collection helper function. Adjusted pathing to account for stylesheet and other files loaded on initial visit.

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -10,13 +10,19 @@ exports.headers = headers = {
   'Content-Type': "text/html"
 };
 
-exports.serveAssets = function(res, asset, callback) {
+exports.serveAssets = function(res, asset, statusCode, callback) {
   if (asset === '/') {
-    asset = '/index.html';
+    asset = '/public/index.html';
+    statusCode = 200;
+  } else if (asset.indexOf('/public/') === asset.indexOf('/archives/')) {
+    // this is only true when both equal -1; in other words, only
+    // when trying to load the stylesheet etc. for our visitors
+    asset = '/public/' + asset;
+    statusCode = 200;
   }
 
   // where is this precise module located with __dirname?
-  var assetPath = path.join(__dirname, '/public/' + asset);
+  var assetPath = path.join(__dirname, asset);
   fs.readFile(assetPath, function(err, data) {
     // ignore error case for now
     // we have data and want to do something with it
@@ -26,23 +32,50 @@ exports.serveAssets = function(res, asset, callback) {
     } else {
       headers['Content-Type'] = 'text/html'; 
     }
-    res.writeHead(200, headers);
-    res.end(data); 
+    // res.writeHead(statusCode, headers);
+    // res.end(data); 
+    exports.sendResponse(res, data, statusCode)
   });
 };
 
 exports.sendResponse = function(response, data, statusCode) {
   statusCode = statusCode || 200;
   response.writeHead(statusCode, headers);
-  response.end(JSON.stringify(data));
+  response.end(data);
 };
 
-exports.collectData = function(request) {
-  var data = "";
+exports.collectData = function(request, response) {
+  var target = "";
   request.on('data', function(chunk) {
-    data += chunk;
+    target += chunk;
   });
   request.on('end', function() {
-
+    target = target.substr(4);
+    exports.processData(target, response);
   });
 };
+
+exports.processData = function(target, response) {
+  archive.isUrlInList(target, function(inList) {
+    if(!inList) {
+      // url is not in list so...
+      // add the url to list
+      archive.addUrlToList(target, function() {
+        //then send user to loading page
+        exports.serveAssets(response, '/public/loading.html', 302);
+      });
+    } else {
+      // url is in list
+      archive.isUrlArchived(target, function(inArchive) {
+        if(!inArchive) {
+          // but url hasn't been archived yet,
+          // so send user to loading page
+          exports.serveAssets(response, '../public/loading.html', 302);
+        } else {
+          // url has been archived, so serve up the archived page
+          exports.serveAssets(response, '../archives/sites/'+target, 200);
+        }
+      });
+    }
+  });
+}
