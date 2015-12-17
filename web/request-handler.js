f95cf98e2f8b89ea9e2cbff18c86cb63a11d3fd8
s-law <slaw@nuclearanvil.com>
2015-12-17 12:34:21 -0800

    Removed a bunch of no longer necessary code that was commented out.

--- web/request-handler.js
+++ web/request-handler.js
@@ -9,32 +9,14 @@ exports.handleRequest = function(req, res) {
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  var parsed = url.parse(req.url, true);
-  var route = parsed.pathname;
 
   if (req.method === 'OPTIONS') {
     httpHelpers.sendResponse(res, null);
   } else if (req.method === 'GET') {
     httpHelpers.serveAssets(res, req.url);
   } else if (req.method === 'POST') {
-    httpHelpers.collectData(req);
-  //   var dataHolder = '';
-  //   req.on('data', function(data) {
-  //     dataHolder += data;
-  //   });
-
-  //   req.on('end', function() {
-  //     dataStore.results.push(JSON.parse(dataHolder));
-
-  //     // NOTE: writes are being APPENDED. each write is
-  //     // the previously stringified message obj
-  //     fs.appendFile(logPath, '\n' + dataHolder, function (err) {});
-
-  //     response.writeHead(201, headers);
-  //     response.end();
-  //   });
+    httpHelpers.collectData(req, res);
   } else {
     httpHelpers.sendResponse(res, null, 404);
   }
-  // res.end(archive.paths.list);
 };
