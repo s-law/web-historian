b3f6b594c886ba4152365324eed9196c2019792a
Steven Law <slaw@nuclearanvil.com>
2015-12-27 12:41:50 -0800

    Refactored htmlfetcher to make use of functions that return promises.

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,7 +1,12 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
 var archiveHelpers = require('../helpers/archive-helpers');
+var Promise = require('bluebird');
 
-archiveHelpers.readListOfUrls(function(array) {
-  archiveHelpers.downloadUrls(array);
+// archiveHelpers.readListOfUrls(function(array) {
+//   archiveHelpers.downloadUrls(array);
+// });
+archiveHelpers.readListOfUrls()
+.then(function(list) {
+  archiveHelpers.downloadUrls(list);
 });
